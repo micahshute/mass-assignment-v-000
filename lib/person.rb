db71081cd4f04d5711af047040dc3d93cceeab3c
Micah Shute <micah.shute@gmail.com>
2018-07-06 07:24:03 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,15 +1,8 @@
 class Person
 
-  attr_accessor :bio, :age, :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
   def initialize(params)
     params.each{|k,v| self.send("#{k}=", v)}
   end
 
 end
-
-
-
-person = {name: "micah", age: "may", bio: "green"}
-me = Person.new(person)
-puts me.name
-puts me
