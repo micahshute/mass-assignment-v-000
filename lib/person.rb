29f6fb17454a635054ebfa6fde24f8ba734c73c8
Micah Shute <micah.shute@gmail.com>
2018-07-06 07:23:52 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,20 +1,13 @@
 class Person
 
-  # attr_accessor :bio, :age, :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
-  attr_accessor :name, :user_name, :age, :location, :bio
+  attr_accessor :bio, :age, :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
   def initialize(params)
-    params.each{|k,v| self.send(("#{k}="), v)}
+    params.each{|k,v| self.send("#{k}=", v)}
   end
 
 end
 
-class User
-  attr_accessor :name, :user_name, :age, :location, :bio
 
-  def initialize(attributes)
-    attributes.each{|key, value| self.send(("#{key}="), value)}
-  end
-end
 
 person = {name: "micah", age: "may", bio: "green"}
 me = Person.new(person)
